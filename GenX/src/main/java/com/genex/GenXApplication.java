22d49f19f288a41d8c3914ad7a2e2750749f13a6
imsadanala <sadanalansp@gmail.com>
2020-05-23 20:18:51 +0530

    GenX Java Code

---
+++ GenX/src/main/java/com/genex/GenXApplication.java
@@ -0,0 +1,13 @@
+package com.genex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenXApplication.class, args);
+	}
+
+}
